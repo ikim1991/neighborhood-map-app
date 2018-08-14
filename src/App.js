f94b1a9a41d156085af022ba53ac8ba3a5e73777
Chris Kim <ikim1991@gmail.com>
2018-08-14 16:44:15 -0400

    Initialize Markers and InfoWindow

--- src/App.js
+++ src/App.js
@@ -4,10 +4,97 @@ import Map from './Components/Map'
 
 class App extends Component {
 
+  state = {
+    locations: [
+      {
+        id: "4ad4c064f964a52074f820e3",
+        name: "Aviva Centre"
+      },
+      {
+        id: "4b155081f964a520b4b023e3",
+        name: "Scotiabank Arena"
+      },
+      {
+        id: "4ad4c062f964a520f3f720e3",
+        name: "BMO Field"
+      },
+      {
+        id: "4ad4c061f964a520adf720e3",
+        name: "Rogers Centre"
+      },
+      {
+        id: "4ad4c05ef964a520d9f620e3",
+        name: "Royal Ontario Museum"
+      }
+    ],
+    places: [
+      {
+        response: {
+          venue: {
+          id: "4ad4c064f964a52074f820e3",
+          name: "Aviva Centre",
+          location: {
+            lat: 43.77155334201711,
+            lng: -79.51192650368728,
+            }
+          }
+        }
+      },
+      {
+        response: {
+        venue: {
+          id: "4b155081f964a520b4b023e3",
+          name: "Scotiabank Arena",
+          location: {
+            lat: 43.64344617535107,
+            lng: -79.37903988889991,
+          }
+          }
+        }
+      },
+      {
+        response: {
+        venue: {
+          id: "4ad4c062f964a520f3f720e3",
+          name: "BMO Field",
+          location: {
+            lat: 43.6334602365744,
+            lng: -79.41805960865929,
+          }
+          }
+        }
+      },
+      {
+        response: {
+        venue: {
+          id: "4ad4c061f964a520adf720e3",
+          name: "Rogers Centre",
+          location: {
+            lat: 43.64192957603611,
+            lng: -79.38905239105225,
+          }
+          }
+        }
+      },
+      {
+        response: {
+        venue: {
+          id: "4ad4c05ef964a520d9f620e3",
+          name: "Royal Ontario Museum",
+          location: {
+            lat: 43.668367065616046,
+            lng: -79.3948129405877,
+          }
+          }
+        }
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map places={this.state.places} />
       </div>
     );
   }
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -5,22 +5,43 @@ class Map extends Component {
 
   state = {
     mapProps: {
-      center: {lat: 43.6332, lng: -79.4186},
-      zoom: 13,
+      center: {lat: 43.6629, lng: -79.3957},
+      zoom: 12,
       width: '100%',
       height: '100vh'
     }
   }
 
   initMap = () => {
-    new window.google.maps.Map(this.refs.map, {
+    this.map = new window.google.maps.Map(this.refs.map, {
       center: this.state.mapProps.center,
       zoom: this.state.mapProps.zoom
     })
   }
 
+  initMarkers = () => {
+    this.props.places.map((place)=>{
+      let marker = new window.google.maps.Marker({
+        map: this.map,
+        position: place.response.venue.location
+      })
+
+      let infowindow = new window.google.maps.InfoWindow({
+        content: place.response.venue.name
+      })
+
+      marker.addListener('mouseover', function(event){
+        infowindow.open(this.map, marker)
+      })
+      marker.addListener('mouseout', function(event){
+        infowindow.close(this.map, marker)
+      })
+    })
+  }
+
   componentDidMount() {
     this.initMap()
+    this.initMarkers()
   }
 
   render() {
